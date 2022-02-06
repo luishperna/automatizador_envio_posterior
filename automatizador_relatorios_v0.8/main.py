02b6c7e416225ae33c645d145776078b3b4c5c95
Luís <luishperna@gmail.com>
2022-02-06 18:43:04 -0300

    Atualização v0.8

--- automatizador_relatorios_v0.8/main.py
+++ automatizador_relatorios_v0.8/main.py
@@ -1,35 +1,33 @@
-from gerador_pdf import gerar_relatorio
-from tkinter import *
+from tkinter import Tk, Label, Button, Entry, END
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.platypus import Paragraph
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import A4, mm
 
 
-# Listas para salvar as opções do usuario
-lista_bornes_de_passagem = []
-lista_capacitor_cilindrico = []
-lista_disjuntor = []
-lista_fusivel = []
-lista_gavetas = []
-lista_inversor_de_frequencia = []
-lista_modulos_mca = []
-lista_soft_starter = []
-lista_switch_gerenciavel = []
-lista_tampa_traseira = []
-lista_tampa_lateral = []
+# Salvar as opções do usuario incremantadas nas listas
+qtd_bornes = lista_bornes_de_passagem = []
+qtd_capacitor = lista_capacitor_cilindrico = []
+qtd_disjuntor = lista_disjuntor = []
+qtd_fusivel = lista_fusivel = []
+qtd_gavetas = lista_gavetas = []
+qtd_inversor = lista_inversor_de_frequencia = []
+qtd_mca = lista_modulos_mca = []
+qtd_soft = lista_soft_starter = []
+qtd_switch = lista_switch_gerenciavel = []
+qtd_tampa_traseira = lista_tampa_traseira = []
+qtd_tampa_lateral = lista_tampa_lateral = []
 
 
 # Função pega a opção do usuario e trabalha nas condições incrementando nas listas
-# ___Lembrete:___ Remover os prints das condições quando os dados estiverem no pdf
-def input_usuario():
+def input_usuario(evento=None):
     numero = int(escolha.get())
-    print(numero)  # Apenas para verificar o get nos testes
     escolha.delete(0, END)
 
-    if numero == 0:
-        print('Sair...')
-        return
-
-    elif numero == 1:
+    if numero == 1:
         lista_bornes_de_passagem.append(1)
-        quantidade = sum(lista_bornes_de_passagem)
+        quantidade = sum(qtd_bornes)
         print(
             f'Você escolheu => Bornes de passagem\nA quantidade atual => {quantidade}')
 
@@ -97,9 +95,56 @@ def input_usuario():
         print('ERRO: Esse valor não está cadastrado, favor digitar novamente')
 
 
-# Variavel para exibir na interface
-opcoes = ["Sair",
-          "Bornes de passagem",
+# Função para gerar um arquivo em pdf
+def gerar_pdf():
+    dados_tabela = [
+        ["Item", "Descrição", "Qtd"],
+        ["1", "Bornes de passagem", str(sum(qtd_bornes))],
+        ["2", "Capacitor cilíndrico", str(sum(qtd_capacitor))],
+        ["3", "Disjuntor", str(sum(qtd_disjuntor))],
+        ["4", "Fusivel", str(sum(qtd_fusivel))],
+        ["5", "Gavetas", str(sum(qtd_gavetas))],
+        ["6", "Inversor de frequencia", str(sum(qtd_inversor))],
+        ["7", "Modulos mca", str(sum(qtd_mca))],
+        ["8", "Soft-starter", str(sum(qtd_soft))],
+        ["9", "Switch gerenciavel", str(sum(qtd_switch))],
+        ["10", "Tampa traseira", str(sum(qtd_tampa_traseira))],
+        ["11", "Tampa lateral", str(sum(qtd_tampa_lateral))],
+    ]
+
+    # Estilos para os paragrafos
+    titulo_style = ParagraphStyle('Heading1', fontName='Helvetica-Bold',
+                                  fontSize=20, textColor=colors.black, leading=20, alignment=1, spaceAfter=30)
+
+    paragrafo_style = ParagraphStyle('Heading2', fontName='Helvetica',
+                                     fontSize=14, textColor=colors.black, leading=20, alignment=1, spaceAfter=15)
+
+    # Paragrafos do pdf
+    titulo = Paragraph('Relatório - Relação de Envio Posterior', titulo_style)
+    paragrago1 = Paragraph(
+        'Segue abaixo relação de itens:', paragrafo_style)
+
+    # Tabela do pdf já com estilização
+    tabela = Table(dados_tabela, [12*mm, 50*mm, 12*mm], 12*[10*mm])
+    tabela.setStyle(TableStyle([
+        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('ALIGN', (1, 1), (1, -1), 'LEFT'),
+        ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+        ('BOX', (0, 0), (-1, -1), 0.25, None),
+        ('BACKGROUND', (0, 0), (-1, 0), colors.lightgrey),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold')
+    ]))
+
+    # Cria o arquivo .pdf e construi a pagina
+    doc = SimpleDocTemplate("Relatório - Relação de Envio Posterior.pdf", pagesize=A4,
+                            rightMargin=10*mm, leftMargin=10*mm, topMargin=20*mm, bottonMargin=10*mm)
+
+    doc.build([titulo, paragrago1, tabela])
+
+
+# Apenas para exibição na interface
+opcoes = ["Bornes de passagem",
           "Capacitor cilíndrico",
           "Disjuntor",
           "Fusivel",
@@ -113,6 +158,7 @@ opcoes = ["Sair",
 
 
 # Criando Interface
+# home = Tk()
 home = Tk()
 home.title('Automatizador - Envio Posterior de Itens')
 
@@ -124,9 +170,10 @@ texto_orientacao.grid(column=0, row=0, padx=200, pady=20)
 
 # Trabalhando com a lista opcoes na interface
 for item in opcoes:
-    index_e_item = f'{opcoes.index(item)} - {item}'
+    index_e_item = f'{opcoes.index(item) + 1} - {item}'
     textos_opcoes = Label(home, text=index_e_item)
     textos_opcoes.grid(column=0, row=(opcoes.index(item) + 1))
+
 
 espacamento = Label(home, text='')
 espacamento.grid(column=0, row=13)
@@ -149,11 +196,15 @@ texto_emitir.grid(column=0, row=16, pady=20)
 
 
 # Botão executa a função gerar_relatorio() importado de gerador_pdf.py
-botao_emitir = Button(home, text='EMITIR PDF', command=gerar_relatorio)
-botao_emitir.grid(column=0, row=17)
+botao_emitir = Button(home, text='EMITIR PDF', command=gerar_pdf)
+botao_emitir.grid(column=0, row=18)
 
 espacamento2 = Label(home, text='')
-espacamento2.grid(column=0, row=18)
+espacamento2.grid(column=0, row=19)
+
+
+# Ao apertar Enter executa a função input_usuario()
+home.bind('<Return>', input_usuario)
 
 
 # Mantém a interface aberta (loop)
